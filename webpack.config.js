434ac2fdf6ca8404cbc0f8016ab9be63da8e430b
Aymeric Cassard <aymeric.cassard@laposte.net>
2023-01-27 14:01:25 +0100

    merge avec main, changement des configurations pour faire tourner le projet sur ma machine

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
-    .setPublicPath('/pareli/public/build/')
+    .setPublicPath('http://127.0.0.1:80/2emeAnnee/AP/Stage/pareli/public/build/')
     // only needed for CDN's or sub-directory deploy
     .setManifestKeyPrefix('http://127.0.0.1:80/2emeAnnee/AP/Stage/pareli/public/build/')
 
